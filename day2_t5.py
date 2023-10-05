2884125e7d832e93be1aad8ea8be9983911fd8e7
bluemouse <haozhilan1@gmail.com>
2023-10-05 16:22:45 +0800

    111

--- day2_t5.py
+++ day2_t5.py
@@ -4,7 +4,7 @@ def judge(n:int):
         if n / i == int(n / i): #(n / i)前面不加int的话，会默认按浮点来处理，会有小数
             return False
     return True
-if __name__ == '__main__':
+if __name__ == '__main__':  #加上这句就可以在别的文件中只引入judge函数，因为这句话可以判断当前文件是不是源文件
     result = []
     for i in range(101, 201):
         if judge(i):
