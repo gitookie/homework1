7544fd0501ac8ef559fff96a98ffcb6d47680b1e
bluemouse <haozhilan1@gmail.com>
2023-10-05 23:35:13 +0800

    first commit

--- day5_t3.py
+++ day5_t3.py
@@ -1,7 +1,11 @@
 import random
-with open('data.txt', 'w') as file_object:
-    for i in range(100000):
-        if i < 99999:
-            file_object.write(str(random.randint(1, 100)) + '\n')
-        else:
-            file_object.write(str(random.randint(1, 100)))
+
+def create(filename:str):
+    with open(filename, 'w') as file_object:
+        for i in range(100000):
+            if i < 99999:
+                file_object.write(str(random.randint(1, 100)) + '\n')
+            else:
+                file_object.write(str(random.randint(1, 100)))
+
+create('data.txt')
