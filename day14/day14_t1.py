e73d66429bb20d5661723ae8b7e9e6d342fcb8f9
haozhilan1 <haozhilan1@gmail.com>
2023-10-24 00:32:01 +0800

    second commit

--- day14/day14_t1.py
+++ day14/day14_t1.py
@@ -8,9 +8,10 @@ import time
 
 all_folder_name = ['aca', 'dem', 'fic', 'news']
 common_path = os.path.join(os.getcwd(), 'day14')
-for name in all_folder_name:
-    os.mkdir(os.path.join(common_path, name))
+"""for name in all_folder_name:
+    os.mkdir(os.path.join(common_path, name))"""
 
+all_result = {}
 
 def timing_decorator(__main__):
     """装饰器，用来记录时间"""
@@ -64,17 +65,56 @@ def process_folder(folder_path):
     
     #print(time)
 
+def process_in_one(folder_path):
+    """把处理以后的结果放到一个.csv文件"""
+    xml_files = glob.glob(os.path.join(folder_path, "*.xml"))
+    #folder_Name = os.path.basename(folder_path)
+    num_of_threads = 15
+    #time = 0
+    #process_xml_file_with_name = functools.partial(process_xml_file_in_one, folder_name = folder_Name)
+    with concurrent.futures.ThreadPoolExecutor(max_workers = num_of_threads) as thread_executor:
+        (thread_executor.map(process_xml_file_in_one, xml_files))
+
+def process_xml_file_in_one(xml_file):
+    """把处理单个xml文件的结果放到一个.csv文件里"""
+    try:
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        words = [elem.get("hw") for elem in root.findall(".//w[@hw]")]
+        #result['filename'] = xml_file
+        #result['word_count'] = len(words)
+        for word in words:
+            if not word in all_result:
+                all_result[word] = 1
+            else:
+                all_result[word] += 1
+        
+        
+    except Exception as e:
+        all_result['filename'] = xml_file
+        all_result['error'] = str(e)
+
 
 @timing_decorator
 def __main__():
     input_dir = "/home/bluemouse/下载/python作业/download/Texts"  # 存放xml文件的目录
     sub_folder = [os.path.join(input_dir, sub_dir) for sub_dir in os.listdir(input_dir) if os.path.isdir(os\
     .path.join(input_dir, sub_dir))]
-
+    #sub_sub = [sub_folder[0]]
     num_processes = 4  # 进程数
 
-    with concurrent.futures.ProcessPoolExecutor(max_workers=num_processes) as process_executor:
-        process_executor.map(process_folder, sub_folder)
+    """with concurrent.futures.ProcessPoolExecutor(max_workers=num_processes) as process_executor:
+        process_executor.map(process_folder, sub_folder)"""
+    """with concurrent.futures.ProcessPoolExecutor(max_workers=num_processes) as process_executor:
+        process_executor.map(process_in_one, sub_sub)"""
+    for i in range(4):
+        process_in_one(sub_folder[i])
+
+    df = pd.DataFrame({'item' : all_result.keys(), 'cnt' : all_result.values()})
+    name = 'all'
+    name += '.csv'
+    result_path = os.path.join(common_path, name)
+    df.to_csv(result_path, index=False)
 
 if __name__ == '__main__':
     __main__()
